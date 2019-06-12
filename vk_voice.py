d9a9ab92d6198ec9b7188c514e3ccd52541e06e6
proninyaroslav <proninyaroslav@gmail.com>
2019-06-12 13:52:54 +0300

    Use docs.getMessagesUploadServer

--- vk_voice.py
+++ vk_voice.py
@@ -14,12 +14,18 @@ _CLIENT_SECRET = "hHbZxrka2uZ6jB1inYsH" # VK for Android app client_secret
 
 _MAX_INT = 2**31-1
 
+def _make_peer_id(user_id, is_chat):
+    return 2000000000 + int(user_id) if is_chat else user_id
+
+def _make_random_id():
+    return random.randint(0, _MAX_INT)
+
 def send(login, password, voice_input, user_id, is_chat=False):
     api = vk_requests.create_api(app_id=_CLIENT_ID,
-				 client_secret=_CLIENT_SECRET,
-				 login=login, password=password,
-				 scope=['messages' 'docs'])
-    upload_url = api.docs.getUploadServer(type='audio_message')['upload_url']
+                                 client_secret=_CLIENT_SECRET,
+                                 login=login, password=password,
+                                 scope=['messages' 'docs'])
+    upload_url = api.docs.getMessagesUploadServer(type='audio_message', peer_id=_make_peer_id(user_id, is_chat))['upload_url']
 
     response = requests.post(upload_url, files=dict(file=voice_input))
     response.raise_for_status()
@@ -27,8 +33,8 @@ def send(login, password, voice_input, user_id, is_chat=False):
     response_file = json.loads(response.text)['file']
     doc_response = api.docs.save(file=response_file)
 
-    message = {'attachment': "doc{0}_{1}".format(doc_response['audio_message']['owner_id'], doc_response['audio_message']['id']),
-	       'random_id': random.randint(0, _MAX_INT)}
+    message = {'attachment': 'doc{0}_{1}'.format(doc_response['audio_message']['owner_id'], doc_response['audio_message']['id']),
+	       'random_id': _make_random_id()}
     if is_chat:
         message['chat_id'] = user_id
     else:
